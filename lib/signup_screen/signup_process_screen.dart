e9f3e8e77389abdf24a2e6367cf801e4eeb9c814
Hamza Shakeel <79325833+HamzaShakeel190@users.noreply.github.com>
2023-09-26 03:12:06 +0500

    Modified signup process screen

--- lib/signup_screen/signup_process_screen.dart
+++ lib/signup_screen/signup_process_screen.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:free_food_delivery_app/signup_screen/singup_screen.dart';
 
 class SignUpProcessScreen extends StatefulWidget {
-
   static const String id = "SignUpProcessScreen";
 
   const SignUpProcessScreen({super.key});
@@ -17,36 +18,52 @@ class _SignUpProcessScreenState extends State<SignUpProcessScreen> {
     var height = MediaQuery.of(context).size.height;
 
     return MaterialApp(
-        theme: ThemeData(
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xff53E88B),
-            ),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff53E88B),
           ),
-          // colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff2ecf80)),
-          useMaterial3: true,
         ),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: SafeArea(
-            child: Column(
-              children: [
-                Align(
+        // colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff2ecf80)),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+
+              // Back Button
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Align(
                   alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                    width: width*0.1,
-                    height: height*0.05,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: const Color(0xFFFDF5ED),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: () {
+                      Navigator.pushNamed(context, SignUpScreen.id);
+                    },
+                    splashColor: const Color(0xE5D77B42),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: const Color(0xFFFDF5ED),
+                      ),
+                      width: 45,
+                      height: 45,
+                      child: const Icon(
+                        CupertinoIcons.back,
+                        color: Color(0xFFDA6317),
+                      ),
                     ),
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+
+            ],
           ),
         ),
+      ),
     );
   }
 }
